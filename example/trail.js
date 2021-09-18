62c41689835fb2bf9da4b8c49ad1e67eb80ad53f
rain-kun <rainkun3@protonmail.com>
2021-09-19 03:43:11 +0530

    single trail with options

---
+++ example/trail.js
@@ -0,0 +1,3 @@
+function Trail(){
+  console.log("working");
+}
---
+++ modules/shapes.js
@@ -0,0 +1,22 @@
+export const circle = `
+.trail {
+  background-color: lightblue;
+  width: .3rem;
+  height: .3rem;
+  border-radius: 50% ;
+}
+`
+export const animStyles = `
+.anim {
+  animation: disapere 1s ease-out forwards;
+}
+
+@keyframes disapere {
+0% {
+  opacity: 1;
+}
+
+100% {
+  opacity: 0;
+}
+}`
